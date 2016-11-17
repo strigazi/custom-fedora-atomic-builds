a35688efab960dbcf6bc9d6df502c3db2a998a8f
Spyros Trigazis <strigazi@gmail.com>
2016-11-17 14:11:38 +0100

    Add install-deps file

---
+++ install-deps.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+dnf install -y \
+@development-tools \
+createrepo \
+kpartx \
+git \
+python-backports-lzma \
+python-devel \
+python-pip \
+python-yaml \
+sudo \
+rpm-build \
+rpm-ostree-toolbox \
+@virtualization \
+yum \
+yum-utils
+
